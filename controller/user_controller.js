d9c896e4207880b41372730ca3aeec92373e8ae6
Tribhuwan <mathincosmos@gmail.com>
2023-07-12 17:36:54 +0530

    render error on logout

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -2,8 +2,11 @@ const Post = require("../models/post");
 const User = require("../models/user");
 
 exports.getUserDetails = async function (req, res, next) {
-  const id = req.params.id;
-  const creator = await User.findById(id);
-  const posts = await Post.find({ user: id });
-  res.render("user", { creator, posts, title: creator.username });
+  if (req.user) {
+    const id = req.params.id;
+    const creator = await User.findById(id);
+    const posts = await Post.find({ user: id });
+    res.render("user", { creator, posts, title: creator.username });
+  }
+  next(new Error("Unauthorized Access"));
 };
